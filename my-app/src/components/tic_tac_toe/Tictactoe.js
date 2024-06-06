d0107a77fe28d51150fc7a9bc04caf104661fce2
henry310101 <4A7G0094@stust.edu.tw>
2024-06-06 15:40:17 +0800

    20240606 tictactoe moves

--- my-app/src/components/tic_tac_toe/Tictactoe.js
+++ my-app/src/components/tic_tac_toe/Tictactoe.js
@@ -2,16 +2,36 @@ import React, {useState} from "react";
 import Board from "./Board";
 
 function Tictactoe() {
+    
     const [history, setHistory] = useState([Array(9).fill(null)]);
     const [currentMove, setCurrentMove] = useState(0);
-    const currentSquares = history[currentMove];
     const xIsNext = currentMove % 2 === 0;
+    const currentSquares = history[currentMove];
 
     const handlePlay = (nextSquares) => {
         const newHistory = [...history.slice(0, currentMove + 1), nextSquares];
         setHistory(newHistory);
         setCurrentMove(newHistory.length - 1);
     };
+
+    
+    const moves = history.map((squares, move)=>{
+        let description;
+        const jumTo = (nextMove) => setCurrentMove(nextMove);
+        if (move > 0) description = "回到第"+move+"步";
+        else description = "遊戲開始";
+        return(
+            <>
+            <li key={move}>
+                <button onClick={() => jumTo(move)}>{description}</button> 
+            </li> 
+            </>
+
+        );
+
+    }
+    );
+
     return (
         <div className="game">
             <div className="game-board">
@@ -19,7 +39,7 @@ function Tictactoe() {
             </div>
             <div className="game-info">
                 <h4>遊戲歷程</h4>
-
+                <ul>{moves}</ul>
             </div>
         </div>
     );
