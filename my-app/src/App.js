6ddc59b3fdd1270d0e010f62c5ceaeaa2758ff39
henry310101 <4A7G0094@stust.edu.tw>
2024-11-05 04:12:46 +0800

    new

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -12,8 +12,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>hello react! 20240509</h1>
-        <h2>react第一個元件</h2>
         <p>
           資工三甲 4A7G0094 朱紘緯
         </p>
